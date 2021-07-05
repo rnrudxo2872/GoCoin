538cb42985849343dd22e5b099cca927436648f7
unknown <rnrudxo2872@naver.com>
2021-07-05 22:16:08 +0900

    Add Block json api(post)

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/rnrudxo2872/GoCoin/blockchain"
+	"github.com/rnrudxo2872/GoCoin/utils"
 )
 
 const port string = ":4000"
@@ -36,14 +39,38 @@ func document(rw http.ResponseWriter, r *http.Request) {
 			Description: "Add A Block",
 			Payload:     "12DQW2F41124",
 		},
+		{
+			URL:         "/blocks/{id}",
+			Method:      "GET",
+			Description: "See A Block",
+		},
 	}
 	fmt.Println(data)
 	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().GetAllBlock())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+
+}
+
+type AddBlockBody struct {
+	Message string
+}
+
 func main() {
 	http.HandleFunc("/", document)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
