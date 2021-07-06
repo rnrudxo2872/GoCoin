5d9b1d943bf70fadaf5800d5cc5730f4020a6c04
unknown <rnrudxo2872@naver.com>
2021-07-07 00:20:47 +0900

    CLI(command line interface) 맛보기

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"github.com/rnrudxo2872/GoCoin/explorer"
-	"github.com/rnrudxo2872/GoCoin/rest"
+	"fmt"
+	"os"
 )
 
-const port string = ":4000"
+func usage() {
+	fmt.Print("반갑습니다! KG 블록 체인을 경험 하시겠어요?\n\n")
+	fmt.Print("뒤에 실행시킬 명령어를 붙혀서 입력해주세요!\n\n")
+	fmt.Println("- explorer : HTML Explorer를 실행합니다.")
+	fmt.Println("- rest : REST API를 실행합니다.(권장)")
+	os.Exit(0)
+}
 
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("explorer 실행!")
+	case "rest":
+		fmt.Println("rest API 실행!")
+	default:
+		usage()
+	}
 }
